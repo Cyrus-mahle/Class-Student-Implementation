6d8a65447b9e56a8d3a88de681f8e6807bd882f8
Cyrus-mahle <67721071+Cyrus-mahle@users.noreply.github.com>
2021-10-18 17:55:21 +0200

    Add files via upload

---
+++ StiudentApp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include"Student.h"
+using namespace std;
+
+int main(){
+ 
+ // dynamic memory allocation(in pointers)
+ Student*object=new Student();// call defaul constructor
+ // dynamic memory allocation(in pointers)	
+ Student*obj=new Student("Mahle","Nyakama",219295387);
+/*
+we have used pointers in this tutorial
+I called print function on my constructor
+*/
+}
+/*
+will inherit class Student on the next tutorial thank you
+subscribe,comment and like */
